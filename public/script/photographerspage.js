db4b72389462aeddb4cc1331838c41ecb5af3530
ribeau <guillaume>
2021-04-27 11:08:07 +0200

    no message

--- public/script/photographerspage.js
+++ public/script/photographerspage.js
@@ -230,7 +230,6 @@ const wrapperDrop =document.querySelector('.custom-select-wrapper')
    wrapperDrop.addEventListener('click', function() { 
     this.querySelector('.custom-select').classList.toggle('open');
     })
-
     wrapperDrop.addEventListener('keydown', function(e) { 
         if(e.key=='ArrowDown'){
       this.querySelector('.custom-select').classList.add('open');
@@ -254,6 +253,19 @@ const wrapperDrop =document.querySelector('.custom-select-wrapper')
      })
  }
 
+
+ for (const option of document.querySelectorAll(".custom-option")) {
+    option.addEventListener('keydown', function(e) {
+        if(e.key == 'Enter'){
+
+        if (!option.classList.contains('selected')) {
+            option.parentNode.querySelector('.custom-option.selected').classList.remove('selected');
+            option.classList.add('selected');
+            option.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = option.textContent;
+           }
+    } 
+  })
+ }
 
 
 
